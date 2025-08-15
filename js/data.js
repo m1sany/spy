5e758de4c7cdec3cb93d8e42fdb4c07c8a55c95c
m1sany <myaako321@gmail.com>
2025-08-15 22:29:56 +0300

    gameCard

--- js/data.js
+++ js/data.js
@@ -3,12 +3,16 @@ document.getElementById('gameSettingsForm').addEventListener('submit', function(
     const players = +document.getElementById('players').value
     const timer = +document.getElementById('timer').value
     const spy = getRandomInt(1, players)
-    console.log(players,timer,spy)
+    const place = ["Аэропорт", "Банк", "Бар", "Больница", "Библиотека", "Военная база", "Гостиница", "Дворец", "Зоопарк", "Казино", "Кинотеатр", "Космическая станция", "Круизный лайнер", "Музей", "Офис", "Океанский лайнер", "Остров", "Парк развлечений", "Пляж", "Подводная лодка", "Полицейский участок", "Ресторан", "Рынок", "Самолёт", "Спа-салон", "Стадион", "Супермаркет", "Театр", "Тюрьма", "Университет", "Ферма", "Церковь", "Цирк", "Школа", "Вокзал", "Гавань", "Гараж", "Джаз-клуб", "Завод", "Кафе", "Киностудия", "Кладбище", "Лаборатория", "Метро", "Монастырь", "Небоскрёб", "Оперный театр", "Паб", "Подземелье", "Стройка"]
+    const placeIndex = getRandomInt(0,place.length)
+    locat = place[placeIndex]
+    console.log(spy, location)
 
     localStorage.setItem('gameSettings', JSON.stringify({
         players,
         timer,
-        spy
+        spy,
+        locat
     }));
     
     window.location.href = 'game.html';
@@ -18,5 +22,3 @@ document.getElementById('gameSettingsForm').addEventListener('submit', function(
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-
