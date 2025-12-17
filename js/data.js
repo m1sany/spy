e7c4cac43c51171d650ac0b397b9530aab581538
m1sany <myaako321@gmail.com>
2025-12-17 23:30:50 +0300

    dfgdgh

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@ document.getElementById('gameSettingsForm').addEventListener('submit', function(
     event.preventDefault()
     const players = +document.getElementById('players').value
     const time = parseInt(document.getElementById('timer').value) 
-    const spy = getRando2mInt(1, players)
+    const spy = getRandomInt(1, players)
     const place = ["Аэропорт", "Банк", "Бар", "Больница", "Библиотека", "Гостиница", "Дворец", "Зоопарк", "Казино", "Кинотеатр", "Космическая станция", "Лайнер", "Музей", "Офис", "Остров", "Парк", "Пляж", "Полицейский участок", "Ресторан", "Рынок", "Самолёт", "Спа-салон", "Стадион", "Супермаркет", "Театр", "Тюрьма", "Университет", "Ферма", "Церковь", "Цирк", "Школа", "Вокзал", "Гавань", "Гараж", "Завод", "Кафе", "Киностудия", "Кладбище", "Лаборатория", "Метро", "Монастырь", "Небоскрёб", "Паб", "Подземелье", "Стройка"]
     const placeIndex = getRandomInt(0,place.length)
     locat = place[placeIndex]
